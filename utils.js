320a31cb21c4a808c44842339bab442fe637183a
Glitch (glitch-hello-website) <none>
2023-08-10 02:14:52 +0000

    🌃🌝 Checkpoint ./match-game.html:57351852/333 ./utils.js:57351852/852

--- utils.js
+++ utils.js
@@ -188,3 +188,34 @@ function resetOverlayAnimation() {
 }
 
 overlay.addEventListener('click', gameStart); 
+
+
+//-------------
+//   resize
+//-------------
+function resizeGame() {
+    const gameWidth =720; // これをゲームの横幅に設定
+    const gameHeight = 950; // これをゲームの縦幅に設定
+
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+
+    const scaleToFitX = windowWidth / gameWidth;
+    const scaleToFitY = windowHeight / gameHeight;
+
+    const currentScreenRatio = windowWidth / windowHeight;
+    const optimalRatio = Math.min(scaleToFitX, scaleToFitY);
+
+    if (currentScreenRatio >= 1.77 && currentScreenRatio <= 1.79) {
+        document.body.style.zoom = optimalRatio;
+    } else {
+        document.body.style.zoom = Math.min(scaleToFitX, scaleToFitY);
+    }
+}
+
+window.addEventListener('resize', resizeGame);
+window.addEventListener('orientationchange', resizeGame);
+
+// 初期ロード時にもリサイズを実行
+window.onload = resizeGame;
+
